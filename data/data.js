3602254bbb16807352c16f330cb7b5862e1f32c5
[deposada74] <[deposada74@misena.edu.co]>
2022-10-18 11:03:46 -0500

    2 commit

---
+++ data/data.js
@@ -0,0 +1,21 @@
+export const data = [
+    {
+        question: "¿cual de las siguientes opciones no es un editor de codigo?",
+        choices: ["vim", "vscode", "atom", "word"],
+        answer: "word",
+    },
+
+    {
+        question: "¿Que lengiaje no es orientado a objetos?",
+        choices: ["java", "haskell", "c++", "python"],
+        answer: "haskell",
+    },
+
+    {
+        question:"¿Que lenguaje no sireve para estilizar un sitio web?",
+        choices: ["stylus", "less", "sass", "PostCSS", "NextCSS"],
+        answer: "NextCSS",
+    },
+]
+
+
